2126a546100b4d08b7d7f5ea36e77a435d1665e2
David_Roark <duyvucrl@gmail.com>
2020-08-08 16:38:51 +0700

    move file to game/, remove class PlayerOne, PlayerTwo, add def position, add class var BOARD, CHOSE

---
+++ ruby/game_tic_tac_toe/game/lib_game.rb
@@ -0,0 +1,48 @@
+
+class GameBoard
+
+  @@BOARD = {
+    '1a' => ' ', '1b' => ' ', '1c' => ' ',
+    '2a' => ' ', '2b' => ' ', '2c' => ' ',
+    '3a' => ' ', '3b' => ' ', '3c' => ' '
+  }
+
+  def self.game_board
+    puts '   a b c'
+    puts '  -------'
+    print "1 |#{@@BOARD['1a']}|#{@@BOARD['1b']}|#{@@BOARD['1c']}|\n"
+    puts '  -------'
+    print "2 |#{@@BOARD['2a']}|#{@@BOARD['2b']}|#{@@BOARD['2c']}|\n"
+    puts '  -------'
+    print "3 |#{@@BOARD['3a']}|#{@@BOARD['3b']}|#{@@BOARD['3c']}|\n"
+    puts '  -------'
+  end
+end
+
+class Player < GameBoard
+  attr_accessor :name_player
+
+  @@CHOSE = []
+
+  def initialize(name_player, mark)
+    @name_player = name_player
+    @mark = mark
+  end
+
+  def self.exist_position?(choose)
+    @@BOARD.has_key?(choose) && @@BOARD[choose] == ' ' ? true : false
+  end
+
+  def win?
+    win = [
+      %w[1a 2a 3a], %w[1b 2b 3b], %w[1c 2c 3c], %w[1a 1b 1c],
+      %w[2a 2b 2c], %w[3a 3b 3c], %w[1a 2b 3c], %w[1c 2b 3a]
+    ]
+    win - @@CHOSE.sort.combination(3).to_a != win
+  end
+
+  def position(choose)
+    @@BOARD[choose] = @mark
+    @@CHOSE << choose
+  end
+end
