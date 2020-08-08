29566fd5b3ce924e89068d25512631bcbd467b40
David_Roark <duyvucrl@gmail.com>
2020-08-08 16:40:49 +0700

    move to game/, change after change lib_game

---
+++ ruby/game_tic_tac_toe/game/main_game.rb
@@ -0,0 +1,50 @@
+load 'lib_game.rb'
+
+puts "Type players's name"
+
+puts "Player 01"
+player01 = gets.chomp
+player1 = Player.new(player01, 'X')
+
+puts "Player 02"
+player02 = gets.chomp
+player2 = Player.new(player02, 'O')
+
+choose = ' '
+player_x = true
+round = 1
+puts "LET'S PLAY!\n"
+
+while (round <= 9 || choose == 'exit') do
+  # Game Board
+  GameBoard.game_board
+
+  while !(Player.exist_position?(choose) || choose == 'exit game') do
+    puts "Your turn, #{ player_x == true ? player1.name_player + '(X)' : player2.name_player + '(O)' }:"
+    print ("Please type your choose position (number-char) like: 1a, 2c... (type 'exit game' if you want to exit). Your choose is: ")
+    choose = gets.chomp
+  end
+
+  break if choose == 'exit game'
+
+  player_x == true ? player1.position(choose) : player2.position(choose)
+
+  player_x = !(player_x)
+  round += 1
+  choose = ' '
+
+  if player1.win?
+    puts "*****~ #{player1.name_player} is the Champion!! ~*****"
+    break
+  end
+
+  if player2.win?
+    puts "*****~ #{player2.name_player} is the Champion!! ~*****"
+    break
+  end
+
+  if round > 9
+    puts '\*/ \*/ This is a draw match \*/ \*/'
+  end
+
+end
